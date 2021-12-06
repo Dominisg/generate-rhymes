7dc642b8dfd6338a97ca488f00a14b32e0b2f2af
Yuriy Kis <yurakis9@gmail.com>
2021-12-06 22:20:56 +0100

    Create frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app id="app">
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <Main />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Main from "./components/Main";
+
+export default {
+  name: "App",
+
+  components: {
+    Main,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+#app {
+  background-color: var(--v-background-base);
+}
+</style>
---
+++ frontend/src/components/Main.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-contaier fill-height fluid>
+    <v-row align="center" justify="center" class="mt-10">
+      <v-col cols="6">
+        <v-row>
+          <v-card width="100%">
+            <v-card-text>
+              <v-row>
+                <v-select
+                  :items="languages"
+                  label="Language"
+                  outlined
+                  class="ma-5"
+                ></v-select>
+                <v-text-field
+                  label="Word"
+                  :rules="rules"
+                  hide-details="auto"
+                  outlined
+                  class="ma-5"
+                ></v-text-field>
+              </v-row>
+              <v-row align="center" justify="center" class="mb-3">
+                <v-btn color="green"> Go!</v-btn>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-row>
+        <v-row class="mt-10">
+          <v-card width="100%">
+            <v-card-text>
+              <v-row> </v-row>
+            </v-card-text>
+          </v-card>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-row> </v-row>
+  </v-contaier>
+</template>
+
+<script>
+export default {
+  name: "Main",
+
+  components: {},
+
+  data: () => ({
+    languages: ["English", "Polish"],
+  }),
+};
+</script>
+
+<style>
+</style>
