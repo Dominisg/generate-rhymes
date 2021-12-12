fef448dc7858335b5d2842f468d3458c1b2a44b4
Dominik Grzegorzek <grzegor.dominik@gmail.com>
2021-12-12 12:19:23 +0100

    by default use dictionary kept in memory

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def disp(language, level, word):
 
 if __name__ == '__main__':
 
-    dicts['en'] = get_dictionary('en', True)
-    dicts['pl'] = get_dictionary('pl', True)
+    dicts['en'] = get_dictionary('en')
+    dicts['pl'] = get_dictionary('pl')
 
     app.run(debug=True)
